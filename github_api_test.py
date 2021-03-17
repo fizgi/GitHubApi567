95fa67a268a9b9f88a346e2453d78feabda4f75c
Fatih IZGI <izgifth@gmail.com>
2021-03-16 21:06:26 -0400

    added mocking

--- github_api_test.py
+++ github_api_test.py
@@ -1,39 +1,37 @@
 """ The primary goal of this file is to demonstrate a simple unittest implementation
 
     author: @fizgi
-    date: 09-Mar-2021
+    date: 16-Mar-2021
     python: v3.9.2
 """
 
 import unittest
+from unittest import mock
 from github_api import get_github_info
 
 
 class TestGithubAPI(unittest.TestCase):
     """ Test class of the methods """
 
-    def test_user_id(self):
+    @mock.patch('github_api.get_github_info')
+    def test_user_id(self, mock_user_id):
         """ test if the request returns a valid response with the given user_id """
-        self.assertFalse(get_github_info('this_is_not_a_valid_user_id'))
-        self.assertFalse(get_github_info('another_non_valid_user_id'))
+        mock_user_id.return_value = False
+        info = get_github_info('this_is_not_a_valid_user_id')
+        self.assertEqual(mock_user_id.return_value, info)
 
-    def test_repo_commit_count(self):
+    @mock.patch('github_api.get_github_info')
+    def test_repo_commit_count(self, mock_commit_count):
+        mock_commit_count.return_value = 30
         repos = get_github_info('richkempinski')
+        commit_count = repos['hellogitworld']
+        self.assertEqual(commit_count, 30)
 
-        self.assertEqual(repos['try_nbdev'], 2)
-        self.assertEqual(repos['try_nbdev2'], 5)
-        self.assertEqual(repos['Mocks'], 10)
-        self.assertEqual(repos['hellogitworld'], 30)
-
-    def test_repo_repository_count(self):
-        richk_repo_count = len(get_github_info('richkempinski'))
-        self.assertEqual(richk_repo_count, 9)
-
-        fizgi_repo_count = len(get_github_info('fizgi'))
-        self.assertEqual(fizgi_repo_count, 29)
-
-        raz_repo_count = len(get_github_info('raziehsaremi'))
-        self.assertEqual(raz_repo_count, 6)
+    @mock.patch('github_api.get_github_info')
+    def test_repo_repository_count(self, mock_repository_count):
+        mock_repository_count.return_value = 9
+        repository_count = len(get_github_info('richkempinski'))
+        self.assertEqual(repository_count, 9)
 
 
 if __name__ == '__main__':
